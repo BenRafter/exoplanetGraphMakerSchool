0cda656bc1fbd099ea49b492b87edfd8f2263213
Benjamin Rafter <brafter@outlook.com>
2021-03-16 14:46:50 -0500

    Initial commit

---
+++ src/dSystemsExoplanetGraphMaker/exoplanetGraphMaker.java
@@ -0,0 +1,13 @@
+package dSystemsExoplanetGraphMaker;
+
+public class exoplanetGraphMaker {
+	public static void print(Object x) {
+		System.out.println(x);
+	}
+	
+	public static void main(String[] args) {
+		print("Running....");
+		
+		print("Done");
+	}
+}
