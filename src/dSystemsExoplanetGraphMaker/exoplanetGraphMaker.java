d7b80afc889d8fdce4b81bf717f44dda3898cccd
Benjamin Rafter <brafter@outlook.com>
2021-03-20 19:15:38 -0500

    Added code to make it so the json data is taken from url and added to a jsonarray when using correct command

--- src/dSystemsExoplanetGraphMaker/exoplanetGraphMaker.java
+++ src/dSystemsExoplanetGraphMaker/exoplanetGraphMaker.java
@@ -3,6 +3,9 @@ package dSystemsExoplanetGraphMaker;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.jsoup.Jsoup;
 
 public class exoplanetGraphMaker {
@@ -18,15 +21,26 @@ public class exoplanetGraphMaker {
 		String url = args[0];
 		url.replaceAll("\"", " ");//Get url and remove " "
 		try {
-			String json = Jsoup.connect(url).ignoreContentType(true).execute().body();
-			print(json);
+			
+			
 			while(!commandQuit.equals(true)) {
 				print("Enter command please");
 				String command = br.readLine();
-				print("Command: " + command);
 				if(command.equals("command:quit")) {
 					print("status:quitting"); 
 					commandQuit = true;
+				}else if(command.equals("command:plot,xAxis:pl_orbper,yAxis:pl_orbeccen")) {
+					print("Doing graph plot");
+					String contents = Jsoup.connect(url).ignoreContentType(true).execute().body();
+					JSONParser parser = new JSONParser();
+					Object obj = parser.parse(contents);
+					JSONArray jsonArray = (JSONArray) obj;
+					for(int i = 0; i < jsonArray.size(); i++) {
+						print(jsonArray.get(i));
+					}
+					//String test = (String) jsonObj.get("pl_name");
+				}else {
+					print("Please enter a valid command");
 				}
 			}
 		}catch (Exception e) {
